cf1abe7ccc247b6c1b98278bfca666e0f3643d3a
Mayowa-Ar <amos.arowolo@gmail.com>
2025-08-03 13:42:22 +0100

    update public_route_table in main.tf

--- main.tf
+++ main.tf
@@ -42,3 +42,17 @@ resource "aws_internet_gateway" "cba_igw" {
   }
 }
 
+resource "aws_route_table" "cba_public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.cba_igw.id
+  }
+
+  tags = {
+    "Name" = "ApachePublicRT"
+  }
+
+}
+
