66e7dc006caf226ad60804fcc1f379b726162080
Mayowa-Ar <amos.arowolo@gmail.com>
2025-08-03 13:26:01 +0100

    update public_subnet in main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,15 @@ resource "aws_vpc" "my_vpc" {
     name = "ApacheVPC"
   }
 }
+
+resource "aws_subnet" "cba_public" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "eu-west-1a"
+
+  tags = {
+    Name = "ApachePublicSubnet"
+  }
+}
+
