dca7049db4915d0e2c3c630847dd8353af91bb44
Mayowa-Ar <amos.arowolo@gmail.com>
2025-08-03 13:28:26 +0100

    update private_subnet in main.tf

--- main.tf
+++ main.tf
@@ -23,3 +23,14 @@ resource "aws_subnet" "cba_public" {
   }
 }
 
+resource "aws_subnet" "cba_private" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone       = "eu-west-1b"
+
+  tags = {
+    Name = "ApachePrivateSubnet"
+  }
+}
+
