1660c203fd273a4b9b0f62849d5ce28c172a9c9a
Mayowa-Ar <amos.arowolo@gmail.com>
2025-08-03 14:14:54 +0100

    update region in output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "web_instance_ip" {
   description = "Web instance complete URL"
   value = join("", ["http://", aws_instance.cba_tf_instance.public_ip])
 }
+
+output "Region" {
+  description = "Region"
+  value       = data.aws_region.current
+}
